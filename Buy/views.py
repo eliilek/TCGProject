27d71bfbfb8f45a57ce1d3f6490846771e394505
eliilek <efklein@iastate.edu>
2018-07-13 15:45:33 -0500

    Debug

--- Buy/views.py
+++ Buy/views.py
@@ -60,64 +60,71 @@ def report_buy(request):
     if request.method != "POST":
         return redirect("/")
     print(request.POST)
-    if 'seller_id' in request.POST.keys():
-        seller = Seller.objects.get(pk=request.POST['seller_id'])
-    else:
-        seller = Seller(
-            name = request.POST['seller_name'],
-            email = request.POST['seller_email'],
-            phone = request.POST['seller_phone']
-        )
-        seller.save()
-    if 'seller_notes' in request.POST.keys() and request.POST['seller_notes'] != "":
-        seller.notes = request.POST['seller_notes']
-        seller.save()
-    block = CardPurchaseBlock(seller=seller, payment_method=request.POST['paymentmethod'])
-    block.save()
-    index = 0
-    errors = ""
-    total_buy = 0
-    bearer = "bearer " + update_bearer().bearer
-    #Regex parse each key named 'card_name_/d', use those indices, catch specific errors
+    try:
+        if 'seller_id' in request.POST.keys():
+            seller = Seller.objects.get(pk=request.POST['seller_id'])
+        else:
+            name_search = Seller.objects.filter(name=request.POST['seller_name'])
+            if len(name_search) == 1:
+                seller = name_search[0]
+            else:
+                seller = Seller(
+                    name = request.POST['seller_name'],
+                    email = request.POST['seller_email'],
+                    phone = request.POST['seller_phone']
+                )
+                seller.save()
+        if 'seller_notes' in request.POST.keys() and request.POST['seller_notes'] != "":
+            seller.notes = request.POST['seller_notes']
+            seller.save()
+        block = CardPurchaseBlock(seller=seller, payment_method=request.POST['paymentmethod'])
+        block.save()
+        index = 0
+        errors = ""
+        total_buy = 0
+        bearer = "bearer " + update_bearer().bearer
+        #Regex parse each key named 'card_name_/d', use those indices, catch specific errors
 
-    for key in request.POST.keys():
-        match = re.match(r'card_name_(\d+)', key)
-        if match:
-            index = match.group(1)
-            if (request.POST['card_name_'+str(index)] != "") and (int(request.POST['quantity_'+str(index)]) > 0):
-                for i in range(0, int(request.POST['quantity_'+str(index)])):
-                    single = SingleCardPurchase(
-                        block = block,
-                        name = request.POST['card_name_'+str(index)],
-                        expansion = request.POST['expansion_'+str(index)],
-                        tcgplayer_card_id = request.POST['tcgplayer_card_id_'+str(index)],
-                        tcgplayer_NM_id = request.POST['tcgplayer_nm_id_'+str(index)],
-                        tcgplayer_LP_id = request.POST['tcgplayer_lp_id_'+str(index)],
-                        buy_price = float(request.POST['price_'+str(index)]),
-                        initial_sell_price = float(request.POST['sell_price_'+str(index)]),
-                        base_price = float(request.POST['sell_price_'+str(index)]),
-                        lowest_listing_at_buy = float(request.POST['lowest_listing_at_buy_'+str(index)]),
-                        lowest_direct_at_buy = float(request.POST['lowest_direct_at_buy_'+str(index)]),
-                        market_price_at_buy = float(request.POST['market_price_'+str(index)])
-                    )
-                    single.save()
-                    total_buy += float(request.POST['price_'+str(index)])
-                if ('auto_list_'+str(index) in request.POST.keys() and request.POST['auto_list_'+str(index)] == "on"):
-                    old_quantity = 0
-                    r = requests.get("http://api.tcgplayer.com/stores/"+os.environ['store_key']+"/inventory/skus/"+single.tcgplayer_card_id+"/quantity", headers={'Authorization':bearer})
-                    if (r.status_code == 200 and r.json()['success']):
-                        old_quantity = int(r.json()['results'][0]['quantity'])
-                    quantity = old_quantity + int(request.POST['quantity_'+str(index)])
-                    r = requests.put("http://api.tcgplayer.com/stores/"+os.environ['store_key']+"/inventory/skus/"+single.tcgplayer_card_id, headers={'Authorization':bearer, 'Content-Type':'application/json'}, json={'price':(single.initial_sell_price * 1.1 if single.initial_sell_price * 1.1 <= single.initial_sell_price + 5 else single.initial_sell_price + 5), 'quantity':quantity, 'channelId':0})
-                    if (r.status_code != 200):
-                        errors += single.name + " price/quantity not updated<br>"
-                    else:
-                        if single.initial_sell_price > 1:
-                            errors += single.name + " to case<br>"
+        for key in request.POST.keys():
+            match = re.match(r'card_name_(\d+)', key)
+            if match:
+                index = match.group(1)
+                if (request.POST['card_name_'+str(index)] != "") and (int(request.POST['quantity_'+str(index)]) > 0):
+                    for i in range(0, int(request.POST['quantity_'+str(index)])):
+                        single = SingleCardPurchase(
+                            block = block,
+                            name = request.POST['card_name_'+str(index)],
+                            expansion = request.POST['expansion_'+str(index)],
+                            tcgplayer_card_id = request.POST['tcgplayer_card_id_'+str(index)],
+                            tcgplayer_NM_id = request.POST['tcgplayer_nm_id_'+str(index)],
+                            tcgplayer_LP_id = request.POST['tcgplayer_lp_id_'+str(index)],
+                            buy_price = float(request.POST['price_'+str(index)]),
+                            initial_sell_price = float(request.POST['sell_price_'+str(index)]),
+                            base_price = float(request.POST['sell_price_'+str(index)]),
+                            lowest_listing_at_buy = float(request.POST['lowest_listing_at_buy_'+str(index)]),
+                            lowest_direct_at_buy = float(request.POST['lowest_direct_at_buy_'+str(index)]),
+                            market_price_at_buy = float(request.POST['market_price_'+str(index)])
+                        )
+                        single.save()
+                        total_buy += float(request.POST['price_'+str(index)])
+                    if ('auto_list_'+str(index) in request.POST.keys() and request.POST['auto_list_'+str(index)] == "on"):
+                        old_quantity = 0
+                        r = requests.get("http://api.tcgplayer.com/stores/"+os.environ['store_key']+"/inventory/skus/"+single.tcgplayer_card_id+"/quantity", headers={'Authorization':bearer})
+                        if (r.status_code == 200 and r.json()['success']):
+                            old_quantity = int(r.json()['results'][0]['quantity'])
+                        quantity = old_quantity + int(request.POST['quantity_'+str(index)])
+                        r = requests.put("http://api.tcgplayer.com/stores/"+os.environ['store_key']+"/inventory/skus/"+single.tcgplayer_card_id, headers={'Authorization':bearer, 'Content-Type':'application/json'}, json={'price':(single.initial_sell_price * 1.1 if single.initial_sell_price * 1.1 <= single.initial_sell_price + 5 else single.initial_sell_price + 5), 'quantity':quantity, 'channelId':0})
+                        if (r.status_code != 200):
+                            errors += single.name + " price/quantity not updated<br>"
                         else:
-                            errors += single.name + " to box<br>"
-                else:
-                    errors += single.name + " not auto-listed<br>"
+                            if single.initial_sell_price > 1:
+                                errors += single.name + " to case<br>"
+                            else:
+                                errors += single.name + " to box<br>"
+                    else:
+                        errors += single.name + " not auto-listed<br>"
+    except Exception as e:
+        print(e)
 
     if request.POST['paymentmethod'] == 'Store Credit':
         errors += "<br>Give " + seller.name + " $" + str(total_buy) + " Store Credit"
